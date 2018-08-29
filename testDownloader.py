09f5fb3af6c37993b6eb28ac2cb109ebe1cafe4c
SeenivasanSeeni <seeni0424@gmail.com>
2018-08-29 10:19:37 +0530

    woring encode

--- testDownloader.py
+++ testDownloader.py
@@ -1,9 +1,9 @@
 import  Downloader as d
 
 def testEncode():
-    product=input("Enter prodcut name")
-    f=d.FlipkartDownloader(product)
+    product=input("Enter prodcut name:")
+    f=d.FlipkartDownloader(product) # encode will be caleed in here.
     print(f.link)
-
+    print("")
 if __name__ == '__main__':
     testEncode()
